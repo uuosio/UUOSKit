825ca82549f425ec7cfee404b6c5d55cf36b93cc
learnforpractice <learnforpractice@gmail.com>
2021-09-28 11:11:06 +0800

    Update setup.py

--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,3 @@
-CC=clang CXX=clang++ python3 setup.py sdist bdist_wheel --plat-name macosx-10.9-x86_64 -- -- -j$(sysctl -n hw.logicalcpu)
+CC=gcc CXX=g++ python3 setup.py sdist bdist_wheel --plat-name macosx-10.9-x86_64 -- -- -j$(sysctl -n hw.logicalcpu)
 
 
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.sysconfig import get_python_lib
 import glob
 
 setup(
-    name="uuoskit",
-    version="0.8.3",
+    name="eoskit",
+    version="0.8.6",
     description="Python Toolkit for EOSIO",
     author='The UUOSIO Team',
     license="MIT",
-    packages=['uuoskit'],
+    packages=['eoskit'],
     # The extra '/' was *only* added to check that scikit-build can handle it.
-    package_dir={'uuoskit': 'pysrc'},
-    package_data={'uuoskit': ['data/*']},
+    package_dir={'eoskit': 'pysrc'},
+    package_data={'eoskit': ['data/*']},
     install_requires=[
         'urllib3>=1.21.1',
         'certifi',
